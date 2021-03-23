601c2bfb80bf34cfd9ca203a2d9e92fddf51863c
Hariharan Ramanathan <hariharan.r1999@gmail.com>
2021-03-23 09:24:23 +0530

    changes made

--- q8/Main.java
+++ q8/Main.java
@@ -1,3 +1,7 @@
+// Get an input from the user through Command line arguments, and print the encrypted or decrypted text depending on the input
+// The input from the user should read as -> Crypto testing key encrypt for encrypting and Crypto /wZf6nsWgQA key decrypt for decrypting(where ascy35;> is the encrypted string). Encrypting and decrypting must be done using the key user gives. Error must be thrown for insufficient input. Use inbuilt or your own encryption logic.
+// TIP: Use Cipher to encrypt/decrypt data if you are using inbuilt classes.
+
 package q8;
 
 import java.security.InvalidKeyException;
@@ -9,7 +13,7 @@ import javax.crypto.NoSuchPaddingException;
 
 
 
-public class code {
+public class Main {
     public static byte[] public_key;
     public static String algo = "AES";
     public static void main(String[] args) throws InvalidKeyException, NoSuchAlgorithmException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException {
